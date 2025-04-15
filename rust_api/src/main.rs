5ea63b89e57047f438c62d471d8af18df634473b
DrNapNap <54890080+DrNapNap@users.noreply.github.com>
2025-04-15 13:06:27 +0200

    static

--- rust_api/src/main.rs
+++ rust_api/src/main.rs
@@ -1,3 +1,21 @@
-fn main() {
-    println!("Hello, world!");
-}
+// use actix_web::{App, HttpServer};
+// use actix_files as fs;
+
+
+// //static
+// #[tokio::main]
+
+// async fn main() -> std::io::Result<()> {
+//     HttpServer::new(|| {
+//         App::new()
+//             .service(fs::Files::new("/static", "./static")
+//                 .show_files_listing())
+//     })
+//     .bind("127.0.0.1:8080")?
+//     .run()
+//     .await
+// }
+
+
+
+
